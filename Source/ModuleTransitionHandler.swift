1099913070a1fda4b7fe02c014fe4ae134aa1a15
rock88 <rock88a@gmail.com>
2017-05-31 22:30:29 +0700

    Add close() to ModuleTransitionHandler

--- Source/ModuleTransitionHandler.swift
+++ Source/ModuleTransitionHandler.swift
@@ -11,6 +11,7 @@ import UIKit
 public protocol ModuleTransitionHandler: class {
     func openModule<View: ModuleView>(factory: ModuleFactory<View>) -> ModuleConfigurator
     func openModule<View: ModuleView>(factory: ModuleFactory<View>) -> ModuleTransition
+    func close()
 }
 
 extension UIViewController: ModuleTransitionHandler {
@@ -22,5 +23,13 @@ extension UIViewController: ModuleTransitionHandler {
     public func openModule<View: ModuleView>(factory: ModuleFactory<View>) -> ModuleTransition {
         let view = factory.instantiateModuleTransitionHandler()
         return ModuleTransitionImpl(source: self, destination: view)
+    public func close() {
+        if let _ = presentingViewController {
+            dismiss(animated: true, completion: nil)
+        } else if let navigationController = navigationController, navigationController.viewControllers.count > 0 {
+            navigationController.popViewController(animated: true)
+        } else {
+            view.removeFromSuperview()
+        }
     }
 }
