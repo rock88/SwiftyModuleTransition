d5fe0f2e687e91d55301a038f1eae00f136f0bd1
rock88 <rock88a@gmail.com>
2017-06-01 21:38:37 +0700

    Make few methods public

--- Source/ModuleOpener.swift
+++ Source/ModuleOpener.swift
@@ -12,12 +12,12 @@ public struct ModuleOpener<View: ModuleView> where View: UIViewController {
     internal let source: UIViewController
     internal let view: View
     
-    func configure(handler: (View.Output) -> Void) -> ModuleTransition<View> {
+    public func configure(handler: (View.Output) -> Void) -> ModuleTransition<View> {
         let configurator = ModuleConfigurator(source: source, view: view)
         return configurator.configure(handler: handler)
     }
     
-    func transition(handler: (UIViewController, UIViewController) -> Void) {
+    public func transition(handler: (UIViewController, UIViewController) -> Void) {
         let transition = ModuleTransition<View>(source: source, destination: view)
         return transition.transition(handler: handler)
     }
